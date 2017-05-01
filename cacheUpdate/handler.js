43a1a5c564e47ed182c6e87532a1e30833824fdb
claudiordgz <claudio.rdgz@gmail.com>
2017-05-01 19:07:04 -0400

    feat: clear output message

--- cacheUpdate/handler.js
+++ cacheUpdate/handler.js
@@ -6,7 +6,11 @@ const configuration = process.env.NODE_ENV
 module.exports.fetchOCReMixAndUpdateCache = (event, context, callback) => {
   ocremix.readOCReMix(configuration)
         .then((val) => {
-          return callback(null, val)
+            let result = "Changed the cache"
+            if (val === false) {
+                result = "No new data to cache"
+            }
+            return callback(null, result)
         })
         .catch((err) => {
           return callback(err)
